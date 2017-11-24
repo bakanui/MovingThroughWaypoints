b92c95a60ffff613ffff5e5affc65d45ebb3f648
rahmadmartin <rahmad.martin@gmail.com>
2017-11-24 07:31:38 +0700

    #car now can move through waypoints but so fast

--- ProgrammingAssignment18/Form1.Designer.vb
+++ ProgrammingAssignment18/Form1.Designer.vb
@@ -22,55 +22,156 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Coordinate = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.StrtBtn = New System.Windows.Forms.Button()
+        Me.StpBtn = New System.Windows.Forms.Button()
+        Me.RstBtn = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SpdBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 41)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(681, 416)
+        Me.PictureBox1.Size = New System.Drawing.Size(672, 292)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'Coordinate
+        'PictureBox2
         '
-        Me.Coordinate.AutoSize = True
-        Me.Coordinate.Location = New System.Drawing.Point(670, 431)
-        Me.Coordinate.Name = "Coordinate"
-        Me.Coordinate.Size = New System.Drawing.Size(23, 13)
-        Me.Coordinate.TabIndex = 1
-        Me.Coordinate.Text = "x, y"
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.PictureBox2.Location = New System.Drawing.Point(41, 175)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(26, 20)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
         '
-        'Button1
+        'StrtBtn
         '
-        Me.Button1.Location = New System.Drawing.Point(790, 300)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.StrtBtn.Location = New System.Drawing.Point(12, 12)
+        Me.StrtBtn.Name = "StrtBtn"
+        Me.StrtBtn.Size = New System.Drawing.Size(75, 23)
+        Me.StrtBtn.TabIndex = 2
+        Me.StrtBtn.Text = "Start"
+        Me.StrtBtn.UseVisualStyleBackColor = True
+        '
+        'StpBtn
+        '
+        Me.StpBtn.Location = New System.Drawing.Point(93, 12)
+        Me.StpBtn.Name = "StpBtn"
+        Me.StpBtn.Size = New System.Drawing.Size(75, 23)
+        Me.StpBtn.TabIndex = 3
+        Me.StpBtn.Text = "Stop"
+        Me.StpBtn.UseVisualStyleBackColor = True
+        '
+        'RstBtn
+        '
+        Me.RstBtn.Location = New System.Drawing.Point(174, 12)
+        Me.RstBtn.Name = "RstBtn"
+        Me.RstBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RstBtn.TabIndex = 4
+        Me.RstBtn.Text = "Reset"
+        Me.RstBtn.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(255, 16)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(42, 17)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "Car"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 8000
+        '
+        'SpdBox
+        '
+        Me.SpdBox.Location = New System.Drawing.Point(432, 14)
+        Me.SpdBox.Name = "SpdBox"
+        Me.SpdBox.Size = New System.Drawing.Size(61, 20)
+        Me.SpdBox.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(387, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Speed"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(716, 75)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(70, 95)
+        Me.ListBox1.TabIndex = 8
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(716, 202)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(29, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(759, 202)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(27, 20)
+        Me.TextBox2.TabIndex = 10
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(885, 522)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Coordinate)
+        Me.ClientSize = New System.Drawing.Size(807, 337)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SpdBox)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RstBtn)
+        Me.Controls.Add(Me.StpBtn)
+        Me.Controls.Add(Me.StrtBtn)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents StrtBtn As System.Windows.Forms.Button
+    Friend WithEvents StpBtn As System.Windows.Forms.Button
+    Friend WithEvents RstBtn As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents SpdBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
 
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Coordinate As Label
-    Friend WithEvents Button1 As Button
 End Class
--- ProgrammingAssignment18/Form1.vb
+++ ProgrammingAssignment18/Form1.vb
@@ -1,29 +1,85 @@
 ﻿Public Class Form1
-    Dim X As Integer
-    Dim Y As Integer
-    Dim canvas As Graphics
-    Dim pen As Pen = New Pen(Color.Black)
+    Dim rectList As New List(Of Rectangle)
+    Dim counter As Integer = 0
+    Dim counter2 As Integer = 0
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        canvas = PictureBox1.CreateGraphics()
-        canvas.DrawRectangle(pen, 25, 25, 25, 25)
+
+    Private Sub createWaypoints(ByVal x As Integer, ByVal y As Integer)
+        Dim myGraphics As Graphics = PictureBox1.CreateGraphics
+        If CheckBox1.Checked = True Then
+            Dim klik As Rectangle = New Rectangle(x, y, 0, 0)
+            rectList.Add(klik)
+            If rectList.Count > 0 Then
+                myGraphics.FillRectangle(Brushes.Black, x, y, 8, 8)
+                ListBox1.Items.Add(klik)
+            End If
+        End If
     End Sub
 
-    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles PictureBox1.MouseMove
-        Coordinate.Text = (e.X & ", " & e.Y)
-        X = e.X
-        Y = e.Y
-    End Sub
-
-    Private Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
-
-    End Sub
-
+    'Private Sub PictureBox1_MouseClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseClick
+    '    createWaypoints(e.X, e.Y)
+    'End Sub
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-        canvas.DrawRectangle(pen, X - 5, Y - 5, 10, 10)
+        Dim a1 As Point
+        a1 = PictureBox1.PointToClient(Control.MousePosition)
+        createWaypoints(a1.X, a1.Y)
+        counter = counter + 1
+
+    End Sub
+    Private Sub mouse_move(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        TextBox1.Text = e.X
+        TextBox2.Text = e.Y
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        'PictureBox2.Location = New Point(a3 + 5, a4 + 20)
+        ' x= 41y=175
     End Sub
 
-    Private Sub PictureBox1_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles PictureBox1.Paint
-        e.Graphics.DrawEllipse(Pens.Blue, 5, 5, 20, 20)
+    Private Sub StrtBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StrtBtn.Click
+        Dim a1, a2, a3, a4 As Integer
+        Dim p As Point
+        a1 = rectList(counter2).X
+        a2 = rectList(counter2).Y
+        a3 = PictureBox2.Location.X
+        a4 = PictureBox2.Location.Y
+        While counter2 < counter
+            While rectList(counter2).X <> PictureBox2.Location.X Or rectList(counter2).Y <> PictureBox2.Location.Y
+                If rectList(counter2).X <> PictureBox2.Location.X AndAlso rectList(counter2).X > PictureBox2.Location.X Then
+                    a3 = a3 + 1
+                ElseIf rectList(counter2).X <> PictureBox2.Location.X AndAlso rectList(counter2).X < PictureBox2.Location.X Then
+                    a3 = a3 - 1
+                End If
+                If rectList(counter2).Y <> PictureBox2.Location.Y AndAlso rectList(counter2).Y > PictureBox2.Location.Y Then
+                    a4 = a4 + 1
+                ElseIf rectList(counter2).Y <> PictureBox2.Location.Y AndAlso rectList(counter2).Y < PictureBox2.Location.Y Then
+                    a4 = a4 - 1
+                End If
+                p = New Point(a3, a4)
+                PictureBox2.Location = p
+                Timer1.Start()
+                Timer1.Stop()
+            End While
+            counter2 = counter2 + 1
+        End While
+        PictureBox2.Location = New Point(p.X, p.Y)
+        Timer1.Enabled = True
     End Sub
+
+    Private Sub StpBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StpBtn.Click
+        Timer1.Enabled = False
+    End Sub
+
+    Public Sub RstBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RstBtn.Click
+        PictureBox2.Left = 41
+        PictureBox2.Top = 175
+        PictureBox1.Refresh()
+        rectList.Clear()
+        ListBox1.Items.Clear()
+        Timer1.Enabled = False
+        counter2 = 0
+        counter = 0
+    End Sub
+
+
 End Class
