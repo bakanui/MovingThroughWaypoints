39a3ad55b18ac67602f1616170d20f5f73d905b7
Bhaskara Ida Bagus <gusbhas@gmail.com>
2017-12-23 20:22:24 +0700

    Trying to calculate direction, can't seem to warp head around it

--- ProgrammingAssignment18Extend/Form1.Designer.vb
+++ ProgrammingAssignment18Extend/Form1.Designer.vb
@@ -22,9 +22,131 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.StrtBtn = New System.Windows.Forms.Button()
+        Me.StpBtn = New System.Windows.Forms.Button()
+        Me.RstBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TorqBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Coordinates = New System.Windows.Forms.Label()
+        Me.SpdBox = New System.Windows.Forms.TextBox()
+        Me.moveCar = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 38)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(785, 284)
+        Me.PictureBox1.TabIndex = 16
+        Me.PictureBox1.TabStop = False
+        '
+        'StrtBtn
+        '
+        Me.StrtBtn.Location = New System.Drawing.Point(12, 9)
+        Me.StrtBtn.Name = "StrtBtn"
+        Me.StrtBtn.Size = New System.Drawing.Size(75, 23)
+        Me.StrtBtn.TabIndex = 17
+        Me.StrtBtn.Text = "Start"
+        Me.StrtBtn.UseVisualStyleBackColor = True
+        '
+        'StpBtn
+        '
+        Me.StpBtn.Location = New System.Drawing.Point(93, 9)
+        Me.StpBtn.Name = "StpBtn"
+        Me.StpBtn.Size = New System.Drawing.Size(75, 23)
+        Me.StpBtn.TabIndex = 16
+        Me.StpBtn.Text = "Stop"
+        Me.StpBtn.UseVisualStyleBackColor = True
+        '
+        'RstBtn
+        '
+        Me.RstBtn.Location = New System.Drawing.Point(174, 9)
+        Me.RstBtn.Name = "RstBtn"
+        Me.RstBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RstBtn.TabIndex = 16
+        Me.RstBtn.Text = "Reset"
+        Me.RstBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(439, 14)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Torque"
+        '
+        'TorqBox
+        '
+        Me.TorqBox.Location = New System.Drawing.Point(484, 12)
+        Me.TorqBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.TorqBox.Name = "TorqBox"
+        Me.TorqBox.Size = New System.Drawing.Size(76, 20)
+        Me.TorqBox.TabIndex = 21
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(322, 14)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 20
+        Me.Label1.Text = "Speed"
+        '
+        'Coordinates
+        '
+        Me.Coordinates.AutoSize = True
+        Me.Coordinates.Location = New System.Drawing.Point(743, 22)
+        Me.Coordinates.Name = "Coordinates"
+        Me.Coordinates.Size = New System.Drawing.Size(23, 13)
+        Me.Coordinates.TabIndex = 19
+        Me.Coordinates.Text = "x, y"
+        '
+        'SpdBox
+        '
+        Me.SpdBox.Location = New System.Drawing.Point(364, 12)
+        Me.SpdBox.Name = "SpdBox"
+        Me.SpdBox.Size = New System.Drawing.Size(61, 20)
+        Me.SpdBox.TabIndex = 18
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(807, 334)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TorqBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Coordinates)
+        Me.Controls.Add(Me.SpdBox)
+        Me.Controls.Add(Me.RstBtn)
+        Me.Controls.Add(Me.StpBtn)
+        Me.Controls.Add(Me.StrtBtn)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Moving Through Waypoints"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents StrtBtn As Button
+    Friend WithEvents StpBtn As Button
+    Friend WithEvents RstBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TorqBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Coordinates As Label
+    Friend WithEvents SpdBox As TextBox
+    Friend WithEvents moveCar As Timer
 End Class
--- ProgrammingAssignment18Extend/Form1.vb
+++ ProgrammingAssignment18Extend/Form1.vb
@@ -1,3 +1,67 @@
 ﻿Public Class Form1
+    Dim X, Y As Integer
+    Dim canvas As Graphics
+    Dim V, ax, ay, dir As Integer
+    Dim posx As Integer = 5
+    Dim posy As Integer = 5
+    Dim waypoints = New List(Of Rectangle)
+    Dim car = New Rectangle(posx, posy, 20, 20)
+    Dim speed As Integer
+    Dim decelerate As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        canvas = PictureBox1.CreateGraphics()
+        SpdBox.Text = "1"
+        TorqBox.Text = "0"
+    End Sub
+    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles PictureBox1.MouseMove
+        X = e.X
+        Y = e.Y
+        Coordinates.Text = (X & ", " & Y)
+    End Sub
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Dim waypoint = New Rectangle(X, Y, 10, 10)
+        canvas.DrawRectangle(Pens.Black, X - 5, Y - 5, 10, 10)
+        waypoints.Add(waypoint)
+    End Sub
+    Private Sub PictureBox1_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles PictureBox1.Paint
+        e.Graphics.DrawEllipse(Pens.Blue, car)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles StrtBtn.Click
+        decelerate = False
+        If SpdBox.Text = "" Then
+            MessageBox.Show("The speed box cannot be left empty!", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf SpdBox.Text < 0 Or SpdBox.Text > 100 Then
+            MessageBox.Show("The car can only go from 1-100.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf SpdBox.Text = 0 Then
+            MessageBox.Show("You can't expect a car to move with a speed of 0.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf waypoints.Count = 0 Then
+            MessageBox.Show("The car needs waypoints to pass through. Please input the waypoints.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf TorqBox.Text = "" Then
+            MessageBox.Show("The speed box cannot be left empty!", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            speed = 101 - SpdBox.Text
+            moveCar.Interval = speed
+            moveCar.Enabled = True
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles StpBtn.Click
+        decelerate = True
+    End Sub
+    Private Sub RstBtn_Click(sender As Object, e As EventArgs) Handles RstBtn.Click
+        moveCar.Enabled = False
+        SpdBox.Text = "1"
+        TorqBox.Text = "0"
+        waypoints.clear()
+        PictureBox1.Refresh()
+        decelerate = False
+    End Sub
+    Private Sub moveCar_Tick(sender As Object, e As EventArgs) Handles moveCar.Tick
+        Dim counter As Integer = 0
+        dir = (waypoints(counter).y - posy) / (waypoints(counter).x - posx)
+        Dim Vx As Integer = V * Math.Cos(dir)
+        Dim Vy As Integer = V * Math.Sin(dir)
+        canvas.DrawEllipse(Pens.White, car)
 
+        canvas.DrawEllipse(Pens.Blue, car)
+    End Sub
 End Class
