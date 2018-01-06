ed53a499aba5eaba0153de35ce3072d816d8085c
Bhaskara Ida Bagus <gusbhas@gmail.com>
2018-01-07 01:34:21 +0700

    Remove acceleration because it breaks torque

--- ProgrammingAssignment18Extend/Form1.vb
+++ ProgrammingAssignment18Extend/Form1.vb
@@ -2,13 +2,14 @@
     Dim counter As Integer = 0
     Dim X, Y As Integer
     Dim canvas As Graphics
-    Dim V, dy, dx, vy, vx, a, ax, ay, dir As Double
-    Dim posx As Integer = 0
-    Dim posy As Integer = 0
+    Dim V, dy, dx, vy, vx, a, ax, ay, dir, tempdir As Double
+    Dim posx As Integer = 5
+    Dim posy As Integer = 5
     Dim car As Rectangle
     Dim waypoints = New List(Of Rectangle)
     Dim decelerate As Boolean = False
     Dim Vmax As Integer = 105
+    Dim cross As Boolean
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         canvas = PictureBox1.CreateGraphics()
         SpdBox.Text = "1"
@@ -21,11 +22,11 @@
     End Sub
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         Dim waypoint = New Rectangle(X, Y, 10, 10)
-        canvas.DrawRectangle(Pens.Black, X - 5, Y - 5, 10, 10)
+        canvas.DrawRectangle(Pens.Black, X, Y, 10, 10)
         waypoints.Add(waypoint)
     End Sub
     Private Sub PictureBox1_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles PictureBox1.Paint
-        e.Graphics.DrawEllipse(Pens.Blue, posx + 5, posy + 5, 20, 20)
+        e.Graphics.DrawEllipse(Pens.Blue, posx, posy, 20, 20)
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles StrtBtn.Click
         decelerate = False
@@ -41,7 +42,7 @@
             MessageBox.Show("The speed box cannot be left empty!", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Else
             V = 1
-            a = 1
+            ' a = 0.1
             dir = 0
             moveCar.Interval = 101 - SpdBox.Text
             moveCar.Enabled = True
@@ -57,6 +58,9 @@
         waypoints.clear()
         posx = 5
         posy = 5
+        counter = 0
+        dir = 0
+        tempdir = 0
         canvas.DrawEllipse(Pens.Blue, posx + 5, posy + 5, 10, 10)
         PictureBox1.Refresh()
         decelerate = False
@@ -64,28 +68,42 @@
     Private Sub moveCar_Tick(sender As Object, e As EventArgs) Handles moveCar.Tick
         If counter < waypoints.Count Then
             Dim dirwaypoint As Double
-            dy = waypoints(counter).Y - posy
-            dx = waypoints(counter).X - posx
+            Dim y2 As Integer = waypoints(counter).Y
+            Dim x2 As Integer = waypoints(counter).X
+            dy = y2 - posy
+            dx = x2 - posx
             dirwaypoint = Math.Atan(dy / dx)
+            tempdir = dir
             dir = dir + TorqBox.Text
-            If dir > dirwaypoint Then
+            If dir > dirwaypoint And tempdir < dir Then
+                dir = dirwaypoint
+            ElseIf dir < dirwaypoint And tempdir > dir Then
                 dir = dirwaypoint
             End If
-            If V < Vmax Then
-                V = V + a
-            End If
+            '            If V < Vmax Then
+            '          V = V + a
+            '      End If
             vx = V * Math.Cos(dir)
             vy = V * Math.Sin(dir)
-            canvas.DrawEllipse(Pens.White, posx + 5, posy + 5, 20, 20)
+            canvas.DrawEllipse(Pens.White, posx, posy, 20, 20)
             posx = posx + vx
             posy = posy + vy
             car.Location = New Point(posx, posy)
-            canvas.DrawEllipse(Pens.Blue, posx + 5, posy + 5, 20, 20)
-            If car.IntersectsWith(waypoints(counter)) Then
+            canvas.DrawEllipse(Pens.Blue, posx, posy, 20, 20)
+            If posx = x2 AndAlso posy = y2 Then
                 counter = counter + 1
+                dir = 0
             End If
         Else
             moveCar.Enabled = False
         End If
     End Sub
+
+    Function dirCheck(dir As Double, dirwaypoint As Double)
+        If dir > dirwaypoint Then
+            cross = True
+        ElseIf dir < dirwaypoint Then
+            cross = False
+        End If
+    End Function
 End Class
