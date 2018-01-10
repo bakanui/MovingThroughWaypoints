ec016da8b97cbfcbe40c1991d65cb18b6cfc8f91
Bhaskara Ida Bagus <gusbhas@gmail.com>
2018-01-10 11:13:18 +0700

    Fix acceleration deceleration

--- ProgrammingAssignment18Extend/Form1.vb
+++ ProgrammingAssignment18Extend/Form1.vb
@@ -2,12 +2,13 @@
     Dim counter As Integer = 0
     Dim X, Y As Integer
     Dim canvas As Graphics
-    Dim V, dy, dx, vy, vx, a, ax, ay, dir, tempdir As Double
+    Dim V, dy, dx, vy, vx, a, dir, tempdir As Double
     Dim posx As Integer = 5
     Dim posy As Integer = 5
     Dim waypoints = New List(Of Rectangle)
-    Dim decelerate As Boolean = False
-    Dim Vmax As Integer = 105
+    Dim Vmax As Integer = 50
+    Dim firstTime As Boolean = True
+    Dim accelerate As Boolean = True
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         canvas = PictureBox1.CreateGraphics()
         SpdBox.Text = "1"
@@ -19,19 +20,22 @@
         Coordinates.Text = (X & ", " & Y)
     End Sub
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-        Dim waypoint = New Rectangle(X, Y, 10, 10)
-        canvas.DrawRectangle(Pens.Black, X + 5, Y + 5, 10, 10)
-        waypoints.Add(waypoint)
+        If firstTime = True Then
+            Dim waypoint = New Rectangle(X, Y, 10, 10)
+            canvas.DrawRectangle(Pens.Black, X + 5, Y + 5, 10, 10)
+            waypoints.Add(waypoint)
+        Else
+            MessageBox.Show("Please reset the program first using the Reset Button.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
     End Sub
     Private Sub PictureBox1_Paint(ByVal sender As Object, ByVal e As PaintEventArgs) Handles PictureBox1.Paint
         e.Graphics.DrawEllipse(Pens.Blue, posx, posy, 20, 20)
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles StrtBtn.Click
-        decelerate = False
         If SpdBox.Text = "" Then
             MessageBox.Show("The speed box cannot be left empty!", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         ElseIf SpdBox.Text < 0 Or SpdBox.Text > 100 Then
-            MessageBox.Show("The car can only go from 1-100.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("The car can only go from 1-50.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         ElseIf SpdBox.Text = 0 Then
             MessageBox.Show("You can't expect a car to move with a speed of 0.", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         ElseIf waypoints.Count = 0 Then
@@ -40,12 +44,15 @@
             MessageBox.Show("The speed box cannot be left empty!", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error)
         Else
             V = SpdBox.Text
+            a = 0.1
             dir = 0
             moveCar.Enabled = True
+            firstTime = False
+            accelerate = True
         End If
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles StpBtn.Click
-        moveCar.Enabled = False
+        accelerate = False
     End Sub
     Private Sub RstBtn_Click(sender As Object, e As EventArgs) Handles RstBtn.Click
         moveCar.Enabled = False
@@ -59,7 +66,7 @@
         tempdir = 0
         canvas.DrawEllipse(Pens.Blue, posx + 5, posy + 5, 10, 10)
         PictureBox1.Refresh()
-        decelerate = False
+        firstTime = True
     End Sub
     Private Sub moveCar_Tick(sender As Object, e As EventArgs) Handles moveCar.Tick
         If counter < waypoints.Count Then
@@ -77,9 +84,10 @@
             ElseIf dir < dirwaypoint And tempdir > dir Then
                 dir = dirwaypoint
             End If
-            ' If V < Vmax Then
-            ' V = V + a
-            'End If
+            If V + 69 > dist AndAlso counter = waypoints.Count - 1 Then
+                accelerate = False
+            End If
+            acceldecel(accelerate)
             vx = V * Math.Cos(dir)
             vy = V * Math.Sin(dir)
             canvas.DrawEllipse(Pens.White, posx, posy, 20, 20)
@@ -87,7 +95,7 @@
             posy = posy + vy
             canvas.DrawEllipse(Pens.Blue, posx, posy, 20, 20)
             If V > dist Then
-                canvas.DrawRectangle(Pens.White, waypoints(counter))
+                canvas.DrawRectangle(Pens.White, waypoints(counter).X + 5, waypoints(counter).Y + 5, 10, 10)
                 counter = counter + 1
                 dir = 0
             End If
@@ -95,4 +103,17 @@
             moveCar.Enabled = False
         End If
     End Sub
+    Function acceldecel(accel As Boolean)
+        If accel = True Then
+            If V < Vmax Then
+                V = V + a
+            End If
+        ElseIf accel = False Then
+            V = V - a
+            If V < 0 Then
+                V = 0
+                moveCar.Enabled = False
+            End If
+        End If
+    End Function
 End Class
